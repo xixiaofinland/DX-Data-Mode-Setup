175bc5ee34f8c042d27262adac5fd37a400447d3
Xi Xiao <xi.xiao007@gmail.com>
2018-11-16 15:39:16 +0200

    +

---
+++ createField.js
@@ -0,0 +1,50 @@
+const csv = require('csv-parser')
+const fs = require('fs')
+const results = [];
+
+let objects = new Set();
+fs.createReadStream('./csv-data/fields.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    let fields = [];
+    for (let row of results) {
+      let sField = new SField(row);
+      fields.push(sField);
+    }
+    createFieldData(fields);
+  });
+
+
+function createFieldData(fields) {
+  for (let field of fields) {
+    const util = require('util');
+    const exec = util.promisify(require('child_process').exec);
+
+    createFieldDataByDX();
+    async function createFieldDataByDX() {
+      let command = `sfdx shane:object:field --api ${field.api} -n ${field.label} -t ${field.type} -o ${field.parent} -l 255`;
+      //sfdx shane:object:field --api Game__c -l 255 -n "Game Type" -t Text -o Game__c --noindex
+      console.log(command);
+
+      const {
+        stdout,
+        stderr
+      } = await exec(command);
+
+      console.log('stdout:', stdout);
+      console.log('stderr:', stderr);
+    }
+  }
+}
+
+function SField(obj) {
+  this.label = obj.label;
+  this.plural = obj.plural;
+  this.api = obj.api;
+  this.description = obj.description;
+  this.type = obj.type;
+  this.parent = obj.parent;
+}
+
+//sfdx shane:object:create --label "Game1" --plural "Game1s" --api Game1__c -t custom ---nametype=Text --description="this stores customer's game record1"
---
+++ createObject.js
@@ -0,0 +1,51 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+const results = [];
+
+let objects = new Set();
+fs.createReadStream('./csv-data/objects.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    let objects = [];
+    for (let row of results) {
+      let sobject = new SObject(row);
+      objects.push(sobject);
+    }
+    createObjectData(objects);
+  });
+
+
+function createObjectData(objects) {
+  for (let object of objects) {
+    //sfdx shane:object:create --label "Game1" --plural "Game1s" --api Game1__c -t custom ---nametype=Text --description="this stores customer's game record1"
+    const util = require('util');
+    const exec = util.promisify(require('child_process').exec);
+
+    createObjectDataByDX();
+    async function createObjectDataByDX() {
+      let command = `sfdx shane:object:create --label ${object.label} --plural ${object.plural} --api ${object.api} -t custom --description="${object.description}"`;
+      console.log(command);
+
+      const {
+        stdout,
+        stderr
+      } = await exec(command);
+
+      console.log('Stdout:', stdout);
+      console.log('ERROR: => ', stderr);
+    }
+  }
+}
+
+
+function SObject(obj) {
+  this.label = obj.label;
+  this.plural = obj.plural;
+  this.api = obj.api;
+  this.description = obj.description;
+
+  this.print = function () {
+    console.log('this is obj label: ' + this.label);
+  };
+}
